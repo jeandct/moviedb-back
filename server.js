7e64e3501423613f2a8a97a477b50ed889939a4a
Jean Ducret <jean.dcrt@gmail.com>
2020-11-28 20:55:32 +0100

    add auth

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 const connection = require('./conf');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
@@ -12,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
+    origin: 'http://localhost:3000',
     credentials: true,
   })
 );
@@ -20,6 +23,77 @@ app.use(cookieParser());
 // -- End Middlewares --
 
 // Routes
+
+app.get('/user/:username', (req, res) => {
+  const { username } = req.params;
+  connection.query(
+    'SELECT * FROM users WHERE username = ? ',
+    [username],
+    (err, user) => {
+      if (err) {
+        res.status(500).send('Error collecting user info');
+      } else {
+        res.json(user[0]);
+      }
+    }
+  );
+});
+
+app.post('/register', (req, res) => {
+  const { username, email, password } = req.body;
+  connection.query(
+    'SELECT * FROM users WHERE username = ?',
+    [username],
+    async (err, results) => {
+      if (results.length > 0) {
+        res.status(409).send('User already registered');
+      } else {
+        let hashedPassword = await bcrypt.hash(password, 10);
+        connection.query(
+          'INSERT INTO users(username, email, password) VALUES(?, ?, ?)',
+          [username, email, hashedPassword],
+          (err, results) => {
+            if (err) {
+              console.log(err);
+              res.status(500).send('Error in registering user');
+            } else {
+              res.send('User successfully registered');
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  connection.query(
+    'SELECT * from users WHERE email = ?',
+    [email],
+    async (err, user) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error');
+      } else if (
+        user.length <= 0 ||
+        !(await bcrypt.compare(password, user[0].password))
+      ) {
+        res.status(401).send('username or password is incorrect');
+      } else {
+        const payload = user[0].username;
+        const token = jwt.sign({ payload }, 'secretKey', { expiresIn: '90d' });
+        const cookieOptions = {
+          expiresIn: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000), //in ms
+          httpOnly: false,
+        };
+
+        res.cookie('token', token, cookieOptions);
+        res.status(200).json(user[0]);
+      }
+    }
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening in port ${PORT}`);
